47993ab7aef5901e63c40934876c6a0221b4b19a
Enot Hueda <enot.hueda@gmail.com>
2020-04-14 19:28:11 -0500

    full robofriends ap changed from react to typescript

---
+++ src/containers/App.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+export interface IRobot {
+	name: string;
+	id: number;
+	email: string;
+}
+
+interface IAppProps {
+
+}
+
+interface IAppState {
+	robots: Array<IRobot>;
+	searchfield: string;
+}
+
+
+class App extends React.Component<IAppProps, IAppState> {
+	constructor(props: IAppProps) {
+		super(props);
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+	}
+
+	componentDidMount(): void {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => {this.setState({ robots: users })})
+	}
+
+	onSearchChange = (event: React.SyntheticEvent<HTMLInputElement>): void => {
+		this.setState({ searchfield: event.currentTarget.value})
+	}
+
+	render(): JSX.Element {
+		const { robots, searchfield } = this.state;
+		const filterRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLocaleLowerCase())
+		})
+		return !robots.length ?
+			<h1>Loading</h1> :		
+		(
+			<div className='tc'>
+				<h1 className='f1'>Robofriends</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<Scroll>
+					<CardList robots={filterRobots}/>					
+				</Scroll>
+			</div>	
+		);	
+	}
+}
+
+export default App;
